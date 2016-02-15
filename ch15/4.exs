5efc59660388d3818c10a8af0e9d58d6c6a4d545
Kerry Buckley <kerryjbuckley@gmail.com>
2016-02-15 21:56:26 +0000

    Got ping ring thing working with a separate process to hold pid list

--- ch15/4.exs
+++ ch15/4.exs
@@ -1,21 +1,54 @@
-defmodule TickerRing do
-  @interval 2000
-  @name :ticker
+defmodule TickerList do
+  @name :tickerlist
 
   def start do
-    spawn __MODULE__, :listen, []
+    pid = spawn __MODULE__, :listen, [[]]
+    :global.register_name @name, pid
   end
 
-  def listen next_in_ring \\ self do
+  def register client_pid do
+    send :global.whereis_name(@name), {:register, client_pid}
+  end
+
+  def next_pid client_pid do
+    send :global.whereis_name(@name), {:next_pid, self, client_pid}
     receive do
-      { :tick } ->
-        IO.puts "tock"
-        listen next_in_ring
-    after
-      @interval ->
-        IO.puts "tick"
-        send next_in_ring, { :tick }
-        listen next_in_ring
+      {:ok, ^client_pid, next_pid} -> next_pid
+    end
+  end
+
+  def listen clients do
+    receive do
+      {:register, pid} ->
+        if Enum.empty? clients do
+          send pid, {:tick}
+        end
+        listen clients ++ [pid]
+      {:next_pid, pid, client_pid} ->
+        send pid, {:ok, client_pid, hd clients}
+        listen tl clients ++ [hd clients]
+    end
+  end
+end
+
+defmodule Ticker do
+  @interval 2000
+
+  def start do
+    pid = spawn __MODULE__, :listen, []
+    TickerList.register pid
+  end
+
+  def listen do
+    receive do
+      {:tick} ->
+        pid = TickerList.next_pid self
+        IO.puts "tick from #{inspect self} to #{inspect pid}"
+        :timer.send_after @interval, pid, {:tick}
+        listen
+      message ->
+        IO.puts "Unexpected message #{inspect message}"
+        listen
     end
   end
 end
