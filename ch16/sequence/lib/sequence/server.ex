2d0bad430f5736eb0ac21d0f728270b64fe61ef4
Kerry Buckley <kerryjbuckley@gmail.com>
2016-02-23 21:11:38 +0000

    Add increment to sequence

--- ch16/sequence/lib/sequence/server.ex
+++ ch16/sequence/lib/sequence/server.ex
@@ -4,4 +4,8 @@ defmodule Sequence.Server do
   def handle_call :next, _from, current do
     {:reply, current, current + 1}
   end
+
+  def handle_cast {:increment_by, delta}, current do
+    {:noreply, current + delta}
+  end
 end
